cfb21ced213a86009b1d8a5ceed63502aa332236
Vijay-Nirmal <pon.vijaynirmal@outlook.com>
2018-08-15 23:26:39 +0530

    Added Publications and Interest

--- KnowledgeEngine/Code/GetFacultyDetails.py
+++ KnowledgeEngine/Code/GetFacultyDetails.py
@@ -14,6 +14,7 @@ def getAllPageUrl():
 def getFacultyLink(allPageUrl):
     facultyLink = []
 
+    print("Getting the list of faculties ...", end="\r")
     for pageUrl in allPageUrl:
         soup = BeautifulSoup(urlopen(pageUrl), "lxml")
         facultys = soup.find("div", {"class": "view-content"}).findChildren("div" , recursive=False)
@@ -26,7 +27,7 @@ def populateFacultyDetailsJSON(facultyLink):
     jsonData = {}
 
     for i, faculty in enumerate(facultyLink, 1):
-        print(str(i) + " in " + str(len(facultyLink)) + " -> Progress {:2.1%}".format(i / len(facultyLink)), end="\r")
+        print("Getting the details of faculty " + str(i) + " in " + str(len(facultyLink)) + " -> Progress {:3.1%}".format(i / len(facultyLink)), end="\r")
 
         soup = BeautifulSoup(urlopen(faculty), "lxml")
 
@@ -53,13 +54,24 @@ def populateFacultyDetailsJSON(facultyLink):
         mainContentSoup = soup.find("div", {"class": "field field-name-body field-type-text-with-summary field-label-hidden"})
         
         description = ""
-        
         for tags in mainContentSoup.findChild().findChild().findChildren(recursive=False):
             if tags.name != 'p':
                 break
             description += tags.text + ""
 
-        jsonData[str(i)] = {"name": name, "Email": email, "positions": positions, "Qualification": qualification, "image": image, "description": description.strip()}
+        publicationsSoup = mainContentSoup.findChild().findChild().find("div", {"class": ['view', 'view-biblio-views', 'view-id-biblio_views', 'view-display-id-block_1']})
+        publications = []
+        if publicationsSoup is not None:
+            for table in publicationsSoup.findChildren("tbody"):
+                for rows in table.findChildren(recursive=False):
+                    publications.append(rows.findChildren(recursive=False)[2].text.strip())
+
+        interest = []
+        interestSoup = soup.find("div", {"class": "field field-name-field-faculty-research-interest field-type-taxonomy-term-reference field-label-inline clearfix"})
+        if interestSoup is not None:
+            interest = interestSoup.findChildren(recursive=False)[1].text.split(",")
+
+        jsonData[str(i)] = {"name": name, "Email": email, "positions": positions, "Qualification": qualification, "image": image, "description": description.strip(), "Publications": publications, "Interest": interest}
 
     with open('KnowledgeEngine/Data/FacultyDetails.json', 'w', encoding='utf-8') as outputfile:
         json.dump(jsonData, outputfile, ensure_ascii=False)
@@ -68,6 +80,7 @@ def getFacultyDetails():
     allPageUrl = getAllPageUrl()
     facultyLink = getFacultyLink(allPageUrl)
     populateFacultyDetailsJSON(facultyLink)
+    print(",,,,,,.....................,,,,,,,,,, Completed ,,,,,,,,,,.....................,,,,,,")
 
 if __name__ == '__main__':
     getFacultyDetails()
