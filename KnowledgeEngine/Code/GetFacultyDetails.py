e502c615f9fcce4e99b0b0b8f9d7648ae2a501f9
Vijay-Nirmal <pon.vijaynirmal@outlook.com>
2018-10-24 20:02:03 +0530

    Added Spell checker

--- KnowledgeEngine/Code/GetFacultyDetails.py
+++ KnowledgeEngine/Code/GetFacultyDetails.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from autocorrect import spell
+import regex as re
 import json
 
 def getAllPageUrl():
@@ -39,8 +41,8 @@ def getFacultyLink(allPageUrl):
     print("Getting the list of faculties ...", end="\r")
     for pageUrl in allPageUrl:
         soup = BeautifulSoup(urlopen(pageUrl), "lxml")
-        facultys = soup.find("div", {"class": "view-content"}).findChildren("div" , recursive=False)
-        for faculty in facultys:
+        faculties = soup.find("div", {"class": "view-content"}).findChildren("div" , recursive=False)
+        for faculty in faculties:
             facultyLink.append("https://www.amrita.edu" + faculty.find("a", href=True)['href'])
 
     return facultyLink
@@ -55,6 +57,9 @@ def populateFacultyDetailsJSON(facultyLink):
     
     """
     jsonData = {}
+
+    with open('KnowledgeEngine/Data/CorrectedWords.json') as correctedWordsJSON:
+        correctedWords = json.load(correctedWordsJSON)
 
     for i, faculty in enumerate(facultyLink, 1):
         print("Getting the details of faculty " + str(i) + " in " + str(len(facultyLink)) + " -> Progress {:3.1%}".format(i / len(facultyLink)), end="\r")
@@ -96,15 +101,44 @@ def populateFacultyDetailsJSON(facultyLink):
                 for rows in table.findChildren(recursive=False):
                     publications.append(rows.findChildren(recursive=False)[2].text.strip())
 
-        interest = []
+        interests = []
         interestSoup = soup.find("div", {"class": "field field-name-field-faculty-research-interest field-type-taxonomy-term-reference field-label-inline clearfix"})
         if interestSoup is not None:
-            interest = interestSoup.findChildren(recursive=False)[1].text.split(",")
+            for interest in interestSoup.findChildren(recursive=False)[1].text.split(","):
+                interest = re.sub(r"\p{P}+", "", interest)
+                interest = interest.lower()
+                interests.append(spellCheck(interest, correctedWords))
 
-        jsonData[str(i)] = {"name": name, "Email": email, "positions": positions, "Qualification": qualification, "image": image, "description": description.strip(), "Publications": publications, "Interest": interest}
+        jsonData[str(i)] = {"name": name, "Email": email, "positions": positions, "Qualification": qualification, "image": image, "description": description.strip(), "Publications": publications, "Interest": interests}
 
     with open('KnowledgeEngine/Data/FacultyDetails.json', 'w', encoding='utf-8') as outputfile:
         json.dump(jsonData, outputfile, ensure_ascii=False)
+
+def spellCheck(input, correctedWords):
+    """To correct spelling mistakes. Corrected spelling are stored in a JSON
+    
+    Parameters
+    ----------
+    input : `String`
+        Sentence to check for spelling mistake
+    correctedWords : `dictionary`
+        Dictionary of wrong words - corrected words
+
+    Returns
+    -------
+    correctedSentence : `String`
+        List of all faculties
+
+    """
+
+    correctedSentence = ""
+    for word in input.split():
+        if word in correctedWords:
+            correctedSentence += " " + correctedWords[word]
+        else:
+            correctedSentence += " " + word
+    
+    return correctedSentence.strip().lower()
 
 def getFacultyDetails():
     """Main function to create faculty details database
@@ -114,6 +148,3 @@ def getFacultyDetails():
     facultyLink = getFacultyLink(allPageUrl)
     populateFacultyDetailsJSON(facultyLink)
     print(",,,,,,.....................,,,,,,,,,, Completed ,,,,,,,,,,.....................,,,,,,")
-
-if __name__ == '__main__':
-    getFacultyDetails()
