b9b776a59ae0de0dc6e27823afb5237458a37ae3
Vijay-Nirmal <pon.vijaynirmal@outlook.com>
2018-12-16 15:42:40 +0530

    Added docs for AInstein, ChatBase, SpeechToText, TextToSpeech

--- SpeechEngine/TextToSpeech.py
+++ SpeechEngine/TextToSpeech.py
@@ -1,6 +1,13 @@
 import pyttsx3
 
 def TextToSpeech(text):
+    """Convert given sentence into speech
+
+    Parameters
+    ----------
+    request : str
+        The sentence to be coverted into speech format
+    """
     engine = pyttsx3.init()
     engine.say(text)
     engine.setProperty('rate', 60)
