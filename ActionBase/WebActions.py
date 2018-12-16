e3f5f5094d411e1b4adc035e2aa527c093590ec4
Vijay-Nirmal <pon.vijaynirmal@outlook.com>
2018-12-16 16:19:00 +0530

    Changed default no of words in summarizer

--- ActionBase/WebActions.py
+++ ActionBase/WebActions.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from LanguageEngine.Summarizer import Gensim as gen
 
-def scrapeDescription(item, wordCount = 30):
+def scrapeDescription(item, wordCount = 50):
     url = "https://www.bing.com/search?q=" + item.replace(" ", "+") + "+wikipedia"
     wikiLink = BeautifulSoup(urlopen(url), "lxml").find("ol", {"id": "b_results"}).find("li", {"class": "b_algo"}).find("a", href=True)['href']
     return gen.getSummaryFromURI(wikiLink, wordCount, 5)
